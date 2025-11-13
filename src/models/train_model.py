5829c2431cdd906076e2e359287c90b5d261a415
Ubuntu <ubuntu@ip-172-31-18-227.eu-west-1.compute.internal>
2025-11-13 18:48:47 +0000

    erste version train_model

---
+++ src/models/train_model.py
@@ -0,0 +1,55 @@
+import pandas as pd 
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import GridSearchCV
+import joblib
+import numpy as np
+import os
+
+print(joblib.__version__)
+
+DATA_PATH = 'data/processed_data/'
+MODEL_PATH = 'models/'  
+
+X_train = pd.read_csv(DATA_PATH + 'X_train_scaled.csv')
+X_test = pd.read_csv(DATA_PATH + 'X_test_scaled.csv')
+y_train = pd.read_csv(DATA_PATH + 'y_train.csv')
+y_test = pd.read_csv(DATA_PATH + 'y_test.csv')
+y_train = np.ravel(y_train)
+y_test = np.ravel(y_test)
+
+def train_model():
+    ''' Train Random Forest Regressor model with hyperparameter tuning
+    ''' 
+    rf_regression = ensemble.RandomForestRegressor(n_estimators=100, random_state=42)
+
+    grid_params = {
+        'n_estimators': [50, 100, 200],
+        'max_depth': [None, 10, 20, 30],
+        'min_samples_split': [2, 5, 10],
+        'min_samples_leaf': [1, 2, 4]
+    }
+
+    grid_search = GridSearchCV(
+        estimator=rf_regression,
+        param_grid=grid_params,
+        cv=3,
+        n_jobs=-1,
+        verbose=2
+    )
+
+    grid_search.fit(X_train, y_train)
+    best_params = grid_search.best_params_
+    best_model = grid_search.best_estimator_
+    return best_model, best_params
+
+def main():
+    ''' Main function to train the model and save it
+    '''
+    best_model, best_params = train_model()
+
+    joblib.dump(best_params, os.path.join(MODEL_PATH, "best_params.pkl"))
+    joblib.dump(best_model, os.path.join(MODEL_PATH, "best_model.pkl"))
+
+if __name__ == '__main__':
+    main()
+    print("Model trained and saved successfully.")
